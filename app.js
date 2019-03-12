d0467aac26d81f03a3975743786aa26c1e18c77e
Moritz <22394314+movd@users.noreply.github.com>
2019-03-12 17:21:58 +0100

    Add batch screenshotting of multiple website

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const puppeteer = require('puppeteer')
 
-const takeScreenshot = async () => {
+const websites = [
+  { url: 'https://rrzk.uni-koeln.de/', filename: 'homepage' },
+  { url: 'https://rrzk.uni-koeln.de/aktuelles.html', filename: 'news' },
+  { url: 'https://typo3.uni-koeln.de/typo3-angebote.html', filename: 'typo3-offerings'},
+  { url: 'https://typo3.uni-koeln.de/typo3-links-und-downloads.html', filename: 'typo3-links-and-downlods'}
+]
+
+const takeScreenshot = async (url, filename) => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
 
   await page.setViewport({ width: 1920, height: 1080 })
-  await page.goto('https://rrzk.uni-koeln.de/')
-  await page.screenshot({ path: './screenshot.png', fullPage: true })
+  await page.goto(url)
+  await page.screenshot({ path: './screenshots/' + filename + '.png', fullPage: true }).then(
+    console.log('Screenshot: ' + filename)
+  )
   await page.close()
   await browser.close()
 }
 
-takeScreenshot()
+for (const website of websites) {
+  // console.log(website.url)
+  // console.log(website.filename)
+  takeScreenshot(website.url, website.filename)
+}
